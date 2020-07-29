ffa44753c1f83d52c45ce21e1275c1f7cefd1b79
Ryan Begnaud <ryanbegnaud13@gmail.com>
2020-07-29 23:01:21 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,5 @@
 require "pry"
-def reverse_each_word(string)
-  array = string.split(/ /).reverse
-  new_array = array.collect do |a| array.reverse
-  binding.pry
-end
+def reverse_each_word(sentence)
+sentence.split.collect {|word| word.reverse}.join(" ")
+
 end
